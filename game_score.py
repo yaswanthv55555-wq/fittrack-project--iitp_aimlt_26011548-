48ff021f918506085e2c87c89ac303f95a3d93af
YVOORA <YVOORA@YVOORA-7410.in.oracle.com>
2026-02-07 06:34:21 -0500

    games_score program

---
+++ game_score.py
@@ -0,0 +1,8 @@
+player_name=input("Enter the Player Name:")
+games_played=int(input("Enter No Of Games Played:"))
+score=int(input("Enter The Score:"))
+avg=score/games_played
+print(f"Player:{player_name}")
+print(f"Games Played:{games_played}")
+print(f"Total Score:{score}")
+print(f"Average Score: {avg}")
